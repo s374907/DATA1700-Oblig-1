b1de442860004ce5649d29b5562f8a3143a568bb
s473907 <s374907@oslomet.no>
2023-02-01 07:10:40 +0100

    pog

--- src/main/static/index.js
+++ src/main/static/index.js
@@ -1,22 +1,53 @@
 let billetter = []
+let table = document.getElementById("table")
 function addBillett(){
+    let error = false
     let film = document.getElementById("film").value
     let antall = document.getElementById("antall").value
+    if(!antall){
+      document.getElementById("error.antall").innerHTML = "You need to write"
+      error = true
+    }
     let fornavn = document.getElementById("fornavn").value
+    if(!fornavn){
+      document.getElementById("error.fornavn").innerHTML = "You need to write"
+      error = true
+    }
     let etternavn = document.getElementById("etternavn").value
+    if(!etternavn){
+      document.getElementById("error.etternavn").innerHTML = "You need to write"
+      error = true
+    }
     let telefonnr = document.getElementById("telefonnr").value
+    if(!telefonnr){
+      document.getElementById("error.telefonnr").innerHTML = "You need to write"
+      error = true
+    }
     let epost = document.getElementById("epost").value
+    if(!epost){
+      document.getElementById("error.epost").innerHTML = "You need to write"
+      error = true
+    }
+    if(error){
+      return
+    }
+    let errors = document.getElementsByClassName("error")
+    for (var i = 0; i < errors.length; i++) {
+      errors[i].innerHTML = " ";
+    }
     billetter.push({film: film, antall: antall, fornavn: fornavn,
     etternavn: etternavn, telefonnr: telefonnr, epost: epost})
-    
-var cols = ['film', 'antall', 'fornavn', 'etternavn', 'telefonnr', 'epost'];
-for (var i = 0; i < billetter.length; i++) {
-  $('table').append('<tr></tr>');
-  for (var j = 0; j < cols.length; j++) {
-    $('table tr:last-child').append('<td>' + billetter[i][cols[j]] + '</td>');
+    let row = table.insertRow(-1)
+    for(let thing in billetter[billetter.length - 1]){
+      let cell = row.insertCell(-1)
+      cell.innerHTML = billetter[billetter.length - 1][thing]
+    }
+
   }
-}    
-}
+    
 function slettBillett(){
+  for(let i = 0; i < billetter.length; i++){
+    table.deleteRow(-1)
+  }
     billetter = []
 }
